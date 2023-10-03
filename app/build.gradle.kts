6ded62b5c8c377cffca449675f4e61052453fa4a
Ahmed Al-Aishat <ahmedalaishat@gmail.com>
2023-10-03 16:37:37 +0300

    Migrate from Groovy to Kotlin

---
+++ app/build.gradle.kts
@@ -0,0 +1,126 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.kotlinAndroid)
+    alias(libs.plugins.kotlinSerialization)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.sqldelight)
+    kotlin("kapt")
+    id("kotlin-parcelize")
+}
+
+android {
+    namespace = "com.alaishat.ahmed.themoviedb"
+    compileSdk = libs.versions.targetSdk.get().toInt()
+
+    defaultConfig {
+        applicationId = "com.alaishat.ahmed.themoviedb"
+        minSdk = libs.versions.minSdk.get().toInt()
+        targetSdk = libs.versions.targetSdk.get().toInt()
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.toVersion(libs.versions.java.get())
+        targetCompatibility = JavaVersion.toVersion(libs.versions.java.get())
+    }
+    kotlinOptions {
+        jvmTarget = libs.versions.java.get()
+    }
+    buildFeatures {
+        buildConfig = true
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.composeCompiler.get()
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    // Androidx
+    implementation(libs.core.ktx)
+    implementation(libs.splashscreen)
+    implementation(libs.startup)
+    implementation(libs.paging.runtime.ktx)
+    implementation(libs.lifecycle.runtime.ktx)
+
+    // Compose
+    implementation(platform(libs.compose.bom))
+    implementation(libs.ui)
+    implementation(libs.ui.graphics)
+    implementation(libs.ui.tooling.preview)
+    implementation(libs.material3)
+    // Compose Integrations
+    implementation(libs.activity.compose)
+    implementation(libs.lifecycle.runtime.compose)
+    implementation(libs.lifecycle.viewmodel.compose)
+    implementation(libs.paging.compose)
+    implementation(libs.navigation.compose)
+    implementation(libs.hilt.navigation.compose)
+
+    // Hilt & Hilt Work
+    implementation(libs.hilt.android)
+    implementation(libs.hilt.work)
+    kapt(libs.hilt.android.compiler)
+    kapt(libs.hilt.compiler)
+    // For Robolectric tests
+    testImplementation(libs.hilt.android.testing)
+    kaptTest(libs.hilt.compiler)
+
+    // Kotlin Serialization
+    implementation(libs.kotlinx.serialization.json)
+
+    // Ktor
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.android)
+    implementation(libs.ktor.client.auth)
+    implementation(libs.ktor.client.content.negotiation)
+    implementation(libs.ktor.client.logging)
+    implementation(libs.ktor.serialization.kotlinx.json)
+
+    // SQL Delight
+    implementation(libs.sqldelight.android.driver)
+    implementation(libs.sqldelight.coroutines)
+    implementation(libs.sqldelight.paging)
+
+    // Coil
+    implementation(libs.coil.compose)
+
+    // Logging
+    implementation(libs.timber)
+
+    // Testing
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+    androidTestImplementation(platform(libs.compose.bom))
+    androidTestImplementation(libs.ui.test.junit4)
+    debugImplementation(libs.ui.tooling)
+    debugImplementation(libs.ui.test.manifest)
+}
+
+sqldelight {
+    databases {
+        create("TheMovieDBDatabase") {
+            packageName.set("com.alaishat.ahmed.themoviedb")
+        }
+    }
+}
--- app/src/main/java/com/alaishat/ahmed/themoviedb/feature/search/SearchScreen.kt
+++ app/src/main/java/com/alaishat/ahmed/themoviedb/feature/search/SearchScreen.kt
@@ -17,6 +17,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
@@ -67,6 +68,7 @@ fun SearchRoute(
 }
 
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 private fun SearchScreen(
     searchText: String,
